72c08a9654992b6283b276de00919a84344de246
core.editor <andreeavillatte@gmail.com>
2019-04-23 15:31:25 +0200

    après corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // //quand la page est chargée (ready)
 $(document).ready(function(){
-    // quand on a le focus sur l'input
+    // quand on a le focus sur l'input, on choissise input pour prendre la version plus generalle
     $('input').focus(function(){
-        // on change le style du formulaire
+        // on change le style du formulaire. On choissise THIS car this appele ma fonction en haut, avec input
       $(this).css('border', '1px solid green');
     });
     $('input').blur(function(){
